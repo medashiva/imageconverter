a0c583643c0a55e85c55f473e0a31fee1212514c
ss <tomedashiva@yahoo.in>
2020-09-26 12:29:15 +0530

    code changes

--- jpgtopdf/views.py
+++ jpgtopdf/views.py
@@ -4,8 +4,12 @@ import img2pdf
 from PIL import Image
 import os
 from django.conf import settings
-
+from datetime import datetime
+import time
 from fpdf import FPDF
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, HttpResponseNotFound
+
 pdf = FPDF()
 # Create your views here.
 
@@ -18,15 +22,26 @@ def jpgtopdf(request):
             filename = fs.save(myfile.name, myfile)
             uploaded_file_url = fs.url(filename)
             image = Image.open(settings.MEDIA_ROOT+"/"+filename)
-            file_name_only = filename.split('.')[0]
+            file_name_only = str(time.strftime("%Y%m%d-%H%M%S"))+filename.split('.')[0].replace(" ", "")
             print(file_name_only)
             pdf_path = (settings.MEDIA_ROOT+"/"+file_name_only+".pdf")
+            print("new line for new branch")
 
             pdf_bytes = img2pdf.convert(image.filename)
             file = open(pdf_path, "wb")
             file.write(pdf_bytes)
             image.close()
             file.close()
+            fs = FileSystemStorage()
+            filename = pdf_path
+            if fs.exists(filename):
+                with fs.open(filename) as pdf:
+                    response = HttpResponse(pdf, content_type='application/pdf')
+                    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+                    return response
+            else:
+                return HttpResponseNotFound('The requested pdf was not found in our server.')
             return render(request,'home.html')
         else:
             return render(request,'home.html')
+
